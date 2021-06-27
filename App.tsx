a5eb459f73117c563c6333d6a6579cc745f234ce
SeonHyungJo <seonhyung.jo@gmail.com>
2021-06-28 01:12:14 +0900

    init

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StyleSheet} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Constants from 'expo-constants'; // Constants로 불러온다.
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import WebView from 'react-native-webview';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,10 +18,33 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+      <SafeAreaProvider style={styles.screen}>
+        {/*<Navigation colorScheme={colorScheme} />*/}
+        {/*<StatusBar />*/}
+        {/*<SafeAreaView edges={['right', 'bottom', 'left']}>*/}
+        {/*  <StatusBar backgroundColor={'transparent'} translucent={true} />*/}
+        <StatusBar style="dark" animated translucent/>
+        <WebView
+          originWhitelist={['*']}
+          allowsInlineMediaPlayback
+          startInLoadingState
+          scalesPageToFit
+          javaScriptEnabledAndroid={true}
+          javaScriptEnabled={true}
+          style={{flex: 1}}
+          source={{uri: 'https://zaritalk.com'}}
+        />
+        {/*</SafeAreaView>*/}
       </SafeAreaProvider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight, // Constants의 statusBarHeight 값을 이용한다.
+    paddingBottom: Constants.statusBarHeight, // Constants의 statusBarHeight 값을 이용한다.
+  }
+});
+
+
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,9 +8,14 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {/* <Text style={styles.title}>Tab One</Text> */}
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
+      {/* <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
+      {/* <WebView
+        originWhitelist={['*']}
+        source={{ html: '<h1>This is a static HTML source!</h1>' }}
+      /> */}
+      <WebView source={{ uri: 'https://zaritalk.com' }} />
     </View>
   );
 }
@@ -19,6 +25,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
   title: {
     fontSize: 20,
